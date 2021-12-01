80f2452272d30294f0d2a45eb3502e454530c477
DouglasMarq <douglas.marq.alves@outlook.com>
2021-12-01 16:16:01 -0300

    Trying to inject config (default.ts) into tjest

--- src/core/__test__/index.test.ts
+++ src/core/__test__/index.test.ts
@@ -1,4 +1,19 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import Core from '..';
+import bindContainer from '../../bin/container';
+
 describe('Core test cases', () => {
+    let context: Container;
+    let bot: Core;
 
+    beforeAll(() => {
+        context = bindContainer();
 
+        bot = context.get<Core>(Core);
+    });
+
+    it('Should check if class is instantiated', () => {
+        expect(bot).toBe(typeof Core);
+    });
 });
